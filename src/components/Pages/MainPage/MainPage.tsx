fd42ec0d4fac9a1365074bf463822fc0995b1210
hadiBH <97242311+hadiBH@users.noreply.github.com>
2024-03-28 21:28:43 +0200

    Update MainPage.tsx

--- src/components/Pages/MainPage/MainPage.tsx
+++ src/components/Pages/MainPage/MainPage.tsx
@@ -3,20 +3,15 @@ import { useState } from "react";
 import QuizResponse from "../../../utilis/interfaces/question.interface";
 import Trivia from "../../Trivia/Trivia";
 import { getQuestions } from "../../../utilis/actions/actions.api";
-import { useDispatch } from "react-redux";
-import { UserAction } from "../../../store/slice/user.slice";
-import { DUMMYDATA } from "../../../utilis/interfaces/question.interface";
 const MainPage = () => {
   const [questions, setQuestion] = useState<QuizResponse | null>(null);
-  const dispatch = useDispatch();
 
   const startQuizHandler = async (
     code: string,
     level: string,
     category: string
   ) => {
-    const data = await getQuestions(code, level);
-    dispatch(UserAction.startTest(category));
+    const data = await getQuestions(code, level, category);
     setQuestion(data);
   };
 
